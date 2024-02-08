7f402f562649e81ef0952d842a8c9a686661dd62
Bill Cathey <billcathey@hotmail.com>
2024-02-08 17:41:45 -0500

    Edit index.js to call render() on createRoot, define React in App component

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import './App.css'
 
 function App() {
     return (
         <div className='App'>
-            <h1>Hello World!</h1>
+            <h1>Movie Hub</h1>
         </div>
     )
 }
