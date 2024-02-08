cc4481891a5b41a24053c7a35197c128d3858bad
Bill Cathey <billcathey@hotmail.com>
2024-02-08 11:29:20 -0500

    Create List and Detail views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+class MovieList(generics.ListAPIView):
+    queryset = Movie.objects.all().reverse()
+    serializer_class = MovieSerializer
+
+class MovieDetail(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
