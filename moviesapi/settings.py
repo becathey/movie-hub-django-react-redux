09fc3f7692113458a6b54452a7e7efd182b10b56
Bill Cathey <billcathey@hotmail.com>
2024-02-08 16:53:46 -0500

    Create template

--- moviesapi/settings.py
+++ moviesapi/settings.py
@@ -48,7 +48,9 @@ ROOT_URLCONF = 'moviesapi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
