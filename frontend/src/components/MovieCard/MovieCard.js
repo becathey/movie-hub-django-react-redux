39e3c7149beb9723f59e2dcca7371dddbeb4de33
Bill Cathey <billcathey@hotmail.com>
2024-02-10 16:47:09 -0500

    Create MovieCard component

---
+++ frontend/src/components/MovieCard/MovieCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MovieCard = (props) => {
+    const {data} = props
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                <img src={data.image} className="card-image" />
+                <h2 className="card-title">{data.title}</h2>
+                <p className="card-director">{data.director}</p>
+            </div>
+            <div className="card-footer">
+                <p className="card-genre">{data.genre}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
--- frontend/src/components/MovieList/MovieList.js
+++ frontend/src/components/MovieList/MovieList.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { UseSelector, useSelector } from "react-redux";
 import { getAllMovies } from "../../features/movies/movieSlice";
+import MovieCard from '../MovieCard/MovieCard';
 
 const MovieList = () => {
     const movies = useSelector(getAllMovies)
-    console.log(movies)
+    const renderedMovies = movies.map(movie => (
+        <MovieCard key={movie.id} data={movie} />
+    ))
 
     return (
-        <h2>MovieList</h2>
+        <>
+            <h2>Movies</h2>
+            <div className='movie-container'>{renderedMovies}</div>
+        </>
     )
 }
 
