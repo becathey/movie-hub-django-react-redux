902bd2faf88a6e00d396e43f36ba8fcb28383835
Bill Cathey <billcathey@hotmail.com>
2024-02-10 16:19:11 -0500

    Setup Redux store

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import {store} from './src/features/store'
 import App from './App'
 import './index.css'
 
@@ -8,8 +9,10 @@ ReactDOM.createRoot(
     document.getElementById('root')
 ).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 )
