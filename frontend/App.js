53b8e1033c3814eb3bc2c45064fd516c73f70d60
Bill Cathey <billcathey@hotmail.com>
2024-02-09 19:33:06 -0500

    Create Footer component

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './src/components/Header/Header'
+import Footer from './src/components/Footer/Footer'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
                 <Header />
                 <h1>Movie Hub</h1>
             </main>
+            <Footer />
         </div>
     )
 }
---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => (
+    <div className="footer">
+        <div>Movie Hub</div>
+        <div>&copy;2024 All rights reserved</div>
+    </div>
+)
+
+export default Footer
