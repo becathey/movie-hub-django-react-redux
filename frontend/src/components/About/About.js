152ea5d03bab62908bb7dd885928fdc20d61a5e7
Bill Cathey <billcathey@hotmail.com>
2024-02-09 19:39:41 -0500

    Create Home and About components

---
+++ frontend/src/components/About/About.js
@@ -0,0 +1,9 @@
+import './About.css'
+
+const About = () => (
+    <div className="about">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas ultricies. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Vestibulum lorem sed risus ultricies. Aenean sed adipiscing diam donec adipiscing. Donec et odio pellentesque diam. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Nunc sed id semper risus in hendrerit. At quis risus sed vulputate odio ut enim. Praesent elementum facilisis leo vel fringilla est. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Pulvinar pellentesque habitant morbi tristique senectus. Egestas quis ipsum suspendisse ultrices gravida. Lobortis elementum nibh tellus molestie. Et egestas quis ipsum suspendisse ultrices gravida. Consequat semper viverra nam libero justo laoreet sit amet. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Augue neque gravida in fermentum et sollicitudin. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Mauris nunc congue nisi vitae suscipit tellus mauris a diam.</p>
+    </div>
+)
+
+export default About
